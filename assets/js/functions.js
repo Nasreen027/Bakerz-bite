a0e4aca889067aa1eafb588efbd7957ee7eaac15
Nasreen027 <nasreenmalik429@gmail.com>
2022-12-28 19:03:57 +0500

    kk

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,13 @@
+let cart = localStorage.getItem("cart") || "[]"
+cart = JSON.parse(cart)
+
+//add to cart
+function addToCart(product) {
+    cart = [...cart, product]
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+
 function makeProductColumn(_product = {}){
     return ` <div class="col-md-4 col-sm-6 pd display">
     <div class="shadow-lg" style="width: 25rem;overflow:hidden">
@@ -7,15 +17,16 @@ function makeProductColumn(_product = {}){
 <div class="card-body container-fluid">
 <h5 class="card-title">${_product.name}</h5>
 <p class="card-text">${_product.price}</p>
+<button class='btn-cart' data-product="${_product.name}">add to cart</button>
 </div>
 </div>
 </div>`
 }
 
-function makeProductGrid(_title, _products=[]){
+function makeProductGrid(_title, _products=[], image){
     let gridHtml = `
       <div class="container mt-5">
-      <h2 class="text-center">${_title}</h2>
+      <div style='background:url(${defaultImageUrl}${image})'><h2 class="text-center">${_title}</h2></div>
       <div class="row">`
 
       const _columns = _products.map((item, index)=>{
@@ -31,3 +42,10 @@ function makeProductGrid(_title, _products=[]){
 
 
 }
+
+$(document).on("click", '.btn-cart', function() {
+  const currentButton = $(this)
+  const product = currentButton.attr("data-product")
+  console.log(product)
+  addToCart(product)
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -12,85 +12,140 @@ const dessertsAndPastryContainer = $("#desserts-and-pastry-container")
 const tartsAndCookiesContainer = $("#tarts-and-cookies-container")
 const puffsAndHiTeaContainer = $("#puffs-and-hi-tea-container")
 
+const productCategories = {
+    CAKES:"cakes",
+    BISCUITS:"biscuits",
+    BREAD:"bread",
+    BROWNIES:"brownies",
+    BUNSANDRUSK:"buns-and-rusk",
+    CUPCAKES:"cup-cakes",
+    DESSERTSANDPASTRY:"desserts-and-pastry",
+    GRABANDGO:"grab-and-go",
+    PUFFSANDHITEA:"puffs-and-hi-tea",
+    SAVOURY:"savoury",
+    TARTSANDCOOKIES:"tarts-and-cookies"
+}
 
-// /////////////////
-//////Cakes////////
-//////////////////
+const products = $.getJSON("./data/products.json", function(data){
 
-$.getJSON("./data/cakes.json", function(data){
-    cakesContainer.html(makeProductGrid("Cakes",data))
+    const cakes = data.filter((item) => item.category===productCategories.CAKES)
+    const biscuits = data.filter((item) => item.category===productCategories.BISCUITS)
+    const bread = data.filter((item) => item.category === productCategories.BREAD)
+    const brownies = data.filter((item)=> item.category === productCategories.BROWNIES)
+    const bunsAndRusk = data.filter((item)=> item.category === productCategories.BUNSANDRUSK)
+    const cupCakes = data.filter((item)=> item.category === productCategories.CUPCAKES)
+    const dessertsAndPastry = data.filter((item)=> item.category === productCategories.DESSERTSANDPASTRY)
+    const grabAndGo = data.filter((item)=> item.category === productCategories.GRABANDGO)
+    const puffsAndHiTea = data.filter((item)=> item.category === productCategories.PUFFSANDHITEA)
+    const savoury = data.filter((item)=> item.category === productCategories.SAVOURY)
+    const tartsAndCookies = data.filter((item)=> item.category === productCategories.TARTSANDCOOKIES)
+
+    
+
+   
+
+
+
+    cakesContainer.html(makeProductGrid("Cakes",cakes, "cakes2.jpg"))
+    biscuitsContainer.html(makeProductGrid("Biscuits",biscuits, data))
+    breadContainer.html(makeProductGrid("Breads And Artisanal Breads",bread, data))
+    browniesContainer.html(makeProductGrid("Brownies", brownies, data))
+    bunsAndRuskContainer.html(makeProductGrid("Buns And Rusk", bunsAndRusk, data))
+    cupCakesContainer.html(makeProductGrid("Cupcakes", cupCakes, data))
+    dessertsAndPastryContainer.html(makeProductGrid("Desserts And Pastry", dessertsAndPastry, data))
+    grabAndGoContainer.html(makeProductGrid("Grab And Go", grabAndGo, data))
+    puffsAndHiTeaContainer.html(makeProductGrid("Puffs And Hi Tea", puffsAndHiTea, data))
+    savouryContainer.html(makeProductGrid("Savoury", savoury, data))
+    tartsAndCookiesContainer.html(makeProductGrid("Tarts And Cookies", tartsAndCookies, data))
+
+
+    //searching products
+    const value = "choco"
+
+    const foundProducts = data.filter((item) => {
+        return item.name.toLowerCase().includes(value.toLowerCase())
+    })
+
+    console.log(foundProducts)
+
 })
+
+
+
+
 
 // //////////////
 /////Cup Cakes//
 ////////////////
 
-$.getJSON("./data/cup-cakes.json", function(data){
-    cupCakesContainer.html(makeProductGrid("Cup Cakes",data))
-})
+// $.getJSON("./data/cup-cakes.json", function(data){
+//     cupCakesContainer.html(makeProductGrid("Cup Cakes",data))
+// })
 
 // //////////////
 ////Brownies///
 ////////////////
 
-$.getJSON("./data/brownies.json", function(data){
-    browniesContainer.html(makeProductGrid("Brownies",data))
-})
+// $.getJSON("./data/brownies.json", function(data){
+//     browniesContainer.html(makeProductGrid("Brownies",data))
+// })
 
 // //////////////
 ////Grab And Go//
 //////////////////
 
-$.getJSON("./data/grab-and-go.json", function(data){
-    grabAndGoContainer.html(makeProductGrid("Grab And Go", data))
-})
+// $.getJSON("./data/grab-and-go.json", function(data){
+//     grabAndGoContainer.html(makeProductGrid("Grab And Go", data))
+// })
 
 // /////////////
 ////Biscuits///
 ////////////////
 
-$.getJSON("./data/biscuits.json", function(data){
-    biscuitsContainer.html(makeProductGrid("Biscuits", data))
-})
+// $.getJSON("./data/biscuits.json", function(data){
+  
+// })
 
 // ////////////
 ///Savoury/////
 ///////////////
 
-$.getJSON("./data/savoury.json", function(data){
-    savouryContainer.html(makeProductGrid("Savoury", data))
-})
+// $.getJSON("./data/savoury.json", function(data){
+//     savouryContainer.html(makeProductGrid("Savoury", data))
+// })
 
 
 // ///////////////////////////////
 //////Bread And Artisanal Bread///
 /////////////////////////////////
 
-$.getJSON("./data/bread.json", function(data){
-    breadContainer.html(makeProductGrid("Breads And Artisanal Breads", data))
-})
+// $.getJSON("./data/bread.json", function(data){
+   
+// })
 
 // ///////////////////////
 /////Buns And Rusk////////
 /////////////////////////
 
-$.getJSON("./data/buns-and-rusk.json", function(data){
-    bunsAndRuskContainer.html(makeProductGrid("Buns And Rusk", data))
-})
+// $.getJSON("./data/buns-and-rusk.json", function(data){
+//     bunsAndRuskContainer.html(makeProductGrid("Buns And Rusk", data))
+// })
 
 ////////////////////////////
 /////Desserts And Pastry///
 ///////////////////////////
 
-$.getJSON("./data/desserts-and-pastry.json", function(data){
-    dessertsAndPastryContainer.html(makeProductGrid("Desserts And Pastry", data))
-})
+// $.getJSON("./data/desserts-and-pastry.json", function(data){
+//     dessertsAndPastryContainer.html(makeProductGrid("Desserts And Pastry", data))
+// })
 
 // //////////////////////
 ////Tarts And Cookies/////
 ////////////////////////
 
-$.getJSON("./data/tarts-and-cookies.json", function(data){
-    tartsAndCookiesContainer.html(makeProductGrid("Tarts And Cookies",data))
-})
+// $.getJSON("./data/tarts-and-cookies.json", function(data){
+//     tartsAndCookiesContainer.html(makeProductGrid("Tarts And Cookies",data))
+// })
+
+
 
